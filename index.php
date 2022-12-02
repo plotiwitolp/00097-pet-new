8d6207a20582991db534b7340b243c6e8fb6b38d
plotiwitolp <plotiwitolp@gmail.com>
2022-12-02 18:41:51 +0400

    first

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>00097</title>
+  <link rel="stylesheet" href="./style.css" />
+</head>
+
+<body>
+  <header>
+    <section>
+      <div class="wrapper">
+        <div class="logo-top">
+          <img src="./img/Sprite-Logo.gif" alt="logo">
+        </div>
+        <nav class="nav-top">
+          <ul class="nav-top__list">
+            <li class="nav-top__item"><a href="#">тест 1</a></li>
+            <li class="nav-top__item"><a href="#">тест 2</a></li>
+            <li class="nav-top__item"><a href="#">тест 3</a></li>
+          </ul>
+        </nav>
+      </div>
+    </section>
+  </header>
+  <main>
+    <section>
+      <div class="container">
+        <div class="primer test">
+          <img src="./img/weather/p1.png" class="kenburns-bottom" alt="">
+        </div>
+        <div class="primer test2">
+          <img src="./img/weather/p2.png" class="kenburns-bottom" alt="">
+        </div>
+        <div class="primer test3">
+          <img src="./img/weather/p3.png" class="kenburns-bottom" alt="">
+        </div>
+        <div class="primer test5">
+          <img src="./img/weather/p4.png" class="kenburns-bottom" alt="">
+        </div>
+        <div class="primer test8">
+          <img src="./img/weather/p5.png" class="kenburns-bottom" alt="">
+        </div>
+        <div class="primer test9">
+          <img src="./img/weather/p6.png" class="kenburns-bottom" alt="">
+        </div>
+        <div class="primer test10">
+          <img src="./img/weather/p7.png" class="kenburns-bottom" alt="">
+        </div>
+        <div class="primer test12">
+          <img src="./img/weather/p8.png" class="kenburns-bottom" alt="">
+        </div>
+        <div class="primer test13">
+          <img src="./img/weather/p9.png" class="kenburns-bottom" alt="">
+        </div>
+        <div class="primer test14">
+          <img src="./img/weather/p10.png" class="kenburns-bottom" alt="">
+        </div>
+        <div class="primer test15">
+          <img src="./img/weather/p11.png" class="kenburns-bottom" alt="">
+        </div>
+      </div>
+    </section>
+  </main>
+  <footer>
+    <section>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis maiores expedita voluptatum sit voluptates cum doloribus dolorum ratione aliquid officia quidem aspernatur totam rerum impedit, sunt itaque assumenda reprehenderit quia.
+    </section>
+  </footer>
+  <script src="./script.js"></script>
+</body>
+
+</html>
